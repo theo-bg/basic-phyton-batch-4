265fb8b028b52fac392cc5c8384d0fe335c7a603
theo-bg <theo.benneditson@gmail.com>
2021-02-19 17:52:57 +0700

    pertemuan-1

---
+++ pertemuan-1/math.py
@@ -0,0 +1,29 @@
+x=10
+y=3
+
+# tambah
+z1 = x + y
+print('Hasil Pertambahan adalah {} + {} = {}'.format(x,y,z1))
+
+#kurang
+z2 = x - y
+print('Hasil Pengurangan adalah {} - {} = {}'.format(x,y,z2))
+
+#kali
+z3 = x * y
+print('Hasil Perkalian adalah {} * {} = {}'.format(x,y,z3))
+
+#bagi
+z4 = x / y
+print('Hasil Pembagian adalah {} / {} = {}'.format(x,y,z4))
+
+#mod
+z5 = x % y
+print('Hasil Mod adalah {} % {} = {}'.format(x,y,z5))
+
+#pangkat
+z6 = x ** y
+print('Hasil Pangkat adalah {} ** {} = {}'.format(x,y,z6))
+
+
+
---
+++ pertemuan-1/variable.py
@@ -0,0 +1,4 @@
+x=5
+y="hello world"
+print(x)
+print(y)
