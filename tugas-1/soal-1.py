1752400040a432f1106f3972683107f41592047a
theo-bg <theo.benneditson@gmail.com>
2021-02-19 17:48:20 +0700

    pertemuan 1

--- tugas-1/soal-1.py
+++
@@ -1,6 +0,0 @@
-name = str(input ("Masukkan Nama Anda : "))
-age = int(input("Masukkan Umur Anda : "))
-height = float(input("Masukkan Tinggi Anda: "))
-
-text= "Nama saya {}, umur saya {} tahun dan tinggi saya {} cm".format(name,age,height)
-print(text)
--- tugas-1/soal-2.py
+++
@@ -1,5 +0,0 @@
-radius = float(input("Masukkan jari-jari lingkaran : "))
-phi = 22/7
-
-area = phi * radius * radius
-print("Luas lingkaran dengan jari-jari {} cm adalah {:.2f} cm\u00b2".format(radius,area))
