aea94fe55d4a3d86f7971dca5e7a7dfb819a75fe
theo-bg <theo.benneditson@gmail.com>
2021-02-19 17:53:58 +0700

    pertemuan-2

---
+++ pertemuan-2/list.py
@@ -0,0 +1,14 @@
+mylist=[]
+print(type(mylist))
+
+mylist.append(1)
+mylist.append(1)
+mylist.append(1)
+mylist.append(1)
+print(mylist)
+
+x=[9,8,7,6,5,4]
+print(x[1])
+
+for i in x:
+    print(i)
---
+++ pertemuan-2/string.py
@@ -0,0 +1,19 @@
+a ="Hello,World!"
+print(a[1])
+print(a[2:5])
+print(a[-1])
+print(a[3:])
+print(a[:6])
+print(a[:-1])
+
+print(len(a))
+
+a="hello"
+b="world"
+c=a+b
+print(c)
+
+d=a + " " + b
+print(d)
+
+print(a+"2")
