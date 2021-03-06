5f84a39f91dd0f7c1d9385b5ac6878240e516fed
theo-bg <theo.benneditson@gmail.com>
2021-03-06 16:02:41 +0700

    pertemuan-5

--- pertemuan-5/read_file.py
+++ pertemuan-5/read_file.py
@@ -1,11 +1,25 @@
-r = open("file.txt","r")
+# r = open("file.txt","r")
 
-print(r.read())
+# print(r.read())
 
-r.close()
+# r.close()
+
+# r = open("resource/hello.txt","r")
+
+# print(r.read())
+
+# r.close()
+
+#read per row
 
 r = open("resource/hello.txt","r")
 
-print(r.read())
+temp2= r.readlines()
+# print(temp2)
 
+a = []
+for li in temp2:
+    a.append(li.strip())
+
+print (a[1])
 r.close()
